6524ba931d3975cd3550f275f709feb6bbeb27b3
Teng-Fong Chan <tfchan.cs07g@nctu.edu.tw>
2022-10-09 22:32:35 +0200

    Use dataclass decorator for Record class

--- record.py
+++ record.py
@@ -1,5 +1,6 @@
 import datetime
 from enum import Enum
+from dataclasses import dataclass
 
 import pandas as pd
 
@@ -10,35 +11,21 @@ class RecordType(Enum):
     TRANSFER = 'Transfer'
 
 
+@dataclass
 class Record:
-    def __init__(self,
-                 from_account: str,
-                 to_account: str,
-                 from_amount: int,
-                 to_amount: int,
-                 from_currency: str,
-                 to_currency: str,
-                 categories: list[str],
-                 date: datetime.date,
-                 time: datetime.time = None,
-                 shop: str = None,
-                 title: str = None,
-                 detail: str = None,
-                 project: str = None
-                 ) -> None:
-        self.from_account = from_account
-        self.to_account = to_account
-        self.from_amount = from_amount
-        self.to_amount = to_amount
-        self.from_currency = from_currency
-        self.to_currency = to_currency
-        self.categories = categories
-        self.date = date
-        self.time = time
-        self.shop = shop
-        self.title = title
-        self.detail = detail
-        self.project = project
+    from_account: str
+    to_account: str
+    from_amount: int
+    to_amount: int
+    from_currency: str
+    to_currency: str
+    categories: list[str]
+    date: datetime.date
+    time: datetime.time = None
+    shop: str = None
+    title: str = None
+    detail: str = None
+    project: str = None
 
     @classmethod
     def from_andromoney(cls, record: pd.Series) -> None:
